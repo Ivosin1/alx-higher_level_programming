541f4911baac9064eb26dc4b2d777d724750b323
Ivosin1 <ivshonowo@gmail.com>
2024-01-11 12:13:27 +0100

    A function in C that checks if a singly linked list is a palindrome.

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,42 +1,93 @@
 #include "lists.h"
-#include <string.h>
 #include <stdio.h>
-#include <stdlib.h>
+
+void reverse(listint_t **head);
+int compare_lists(listint_t *head, listint_t *middle, int len);
 
 /**
-  * is_palindrome - A function that checks if a list is a palindrome.
-  * @head: The pointer to the head of the list.
-  *
-  * Return: 0 if list is not a palindrome, and 1 if it is.
-  */
+ * is_palindrome - Function that checks if a singly linked list is a palidrome
+ * @head: The pointer to the head of the list.
+ *
+ * Return: 0 if a palidrome isnt detected and 1 if it is 
+ */
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *tmp = *head;
-	int nodes = 0, i = 0, *array = NULL;
+	int len, i;
+	listint_t *tmp;
+	listint_t *middle;
 
-	if (*head == NULL || head == NULL || (*head)->next == NULL)
+	if (head == NULL || *head == NULL)
 		return (1);
-	while (tmp)
-	{
-		nodes++;
-		tmp = tmp->next;
-	}
-	array = malloc(sizeof(int) * nodes);
 	tmp = *head;
-	while (tmp)
-	{
-		array[i++] = tmp->n;
+	middle = *head;
+
+	for (len = 0; tmp != NULL; len++)
 		tmp = tmp->next;
-	}
-	for (i = 0; i < nodes / 2; i++)
+
+	len = len / 2;
+
+	for (i = 1; i < len; i++)
+		middle = middle->next;
+	if (len % 2 != 0 && len != 1)
 	{
-		if (array[i] != array[nodes - 1 - i])
-		{
-			free(array);
-			return (0);
-		}
+		middle = middle->next;
+		len = len - 1;
 	}
-	free(array);
+	reverse(&middle);
+	i = compare_lists(*head, middle, len);
+
+	return (i);
+}
+
+/**
+ * compare_lists - compare two lists
+ * @head: pointer to the head node
+ * @middle: pointer to the middle node
+ * @len: length of the list
+ * 
+ * Return: if the same 1, if not 0
+ */
+
+int compare_lists(listint_t *head, listint_t *middle, int len)
+{
+	int i;
+
+	if (head == NULL || middle == NULL)
+		return (1);
+	for (i = 0; i < len; i++)
+	{
+		if (head->n != middle->n)
+			return (0);
+		head = head->next;
+		middle = middle->next;
+	}
 	return (1);
+}
+
+/**
+ * reverse - reverse a list
+ * @head: pointer to the head to reverse
+ */
+
+void reverse(listint_t **head)
+{
+	listint_t *current;
+	listint_t *next;
+	listint_t *prev;
+
+	if (head == NULL || *head == NULL)
+		return;
+
+	prev = NULL;
+	current = *head;
+	while (current != NULL)
+	{
+
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	*head = prev;
 }
