881c58b97efc6fdc60207be7e599af7f36dc1fdf
Ivosin1 <ivshonowo@gmail.com>
2024-01-11 06:40:58 +0100

    Secure access to an element in a list task.

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    """Prints all integers of a list"""
-    for i in range(len(my_list)):
-        print('{:d}'.format(my_list[i]))
+def element_at(my_list, idx):
+    if idx < 0:
+        return (None)
+    if idx > len(my_list) - 1:
+        return (None)
+    return my_list[idx]
