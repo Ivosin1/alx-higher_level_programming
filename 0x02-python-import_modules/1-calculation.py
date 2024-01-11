e7556b90d9d7fc80ef4abf323aac49ed136920b4
Ivosin1 <ivshonowo@gmail.com>
2024-01-11 05:42:24 +0100

    My first toolbox task.

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """This prints the sum, difference, multiple & quotient of 10 & 5"""
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
