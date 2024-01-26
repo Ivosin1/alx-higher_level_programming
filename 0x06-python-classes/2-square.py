9135c75fb0a12874985af1f46ba77a757e849ab4
Ivosin1 <ivshonowo@gmail.com>
2024-01-26 04:03:37 +0100

    A class Square that defines a square by: (based on 1-square.py).

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This defines the class 'Square'."""
+
+
+class Square:
+    """THis represents a square."""
+
+    def __init__(self, size=0):
+        """This initializes the Square.
+        Args:
+            size (int): Size of the square."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
