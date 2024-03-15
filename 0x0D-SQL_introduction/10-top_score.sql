9692e5f3bb7600ae920b2b79282ccc6551fac0ab
Ivosin1 <ivshonowo@gmail.com>
2024-03-15 04:05:58 +0100

    A script that lists all records of the table second_table of the database.

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- A script that lists all records of the table 'second_table'.
+-- Results should show only scores displayed in descending order and names.
+
+SELECT score, name FROM second_table ORDER BY score desc;
