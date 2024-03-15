3ea81c5fd6ae7c5ea7202f87b9ab0fc15e6b2544
Ivosin1 <ivshonowo@gmail.com>
2024-03-15 03:48:28 +0100

    A script that creates a table 'second_table' in the database.

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
 -- A script that creates a table 'second_table' in the database.
 
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO TABLE second_table VALUE (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
+INSERT INTO second_table VALUE (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
