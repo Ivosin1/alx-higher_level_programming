5911d0be52c361bb120fb0b54f278e89ca1848e7
Ivosin1 <ivshonowo@gmail.com>
2024-03-15 05:32:58 +0100

    A script that lists all records of the table 'second_table' of the database.

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,5 @@
+-- A script that lists all records of the table second_table of the database
+-- and doesn't list rows without a name
+-- and records are listed by descending score order.
+
+SELECT score, name from second_table WHERE name IS NOT NULL ORDER BY score DESC;
